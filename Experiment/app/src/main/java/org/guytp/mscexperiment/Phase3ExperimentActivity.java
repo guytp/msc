95f9a9730e8fea3e530ed5497652aa109b4b6b9d
Guy Powell <guy@guytp.org>
2017-06-29 15:36:41 +0100

    Added emotion words to use for Phase 3

--- Experiment/app/src/main/java/org/guytp/mscexperiment/Phase3ExperimentActivity.java
+++ Experiment/app/src/main/java/org/guytp/mscexperiment/Phase3ExperimentActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 
 public class Phase3ExperimentActivity extends KioskActivity {
 
+    private final String[] _unsortedEmotionWords = {
+            "Excited", "Happy", "Surprised", "Delighted", "Cheerful", // High Energy, Pleasant
+            "Relaxed", "Calm", "Serene", "Content", "Pleased",  // Low Energy, Pleasant
+            "Sad", "Depressed", "Gloomy", "Bored", "Tired", // Low Energy, Unpleasant
+            "Afraid", "Angry", "Annoyed", "Frustrated", "Terrified" // High Energy, Unpleasant
+            };
+            // Mix of Russell and Schubert : https://www.researchgate.net/figure/228583824_fig10_Figure-10-Ratings-of-emotion-words-in-a-two-dimensional-emotion-space-according-to
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
