2481165d0d255f5bdcfd690257de60d3b64bff15
Guy Powell <guy@guytp.org>
2017-07-03 13:21:14 +0100

    Added support for more than 20 ordinals

--- Experiment/app/src/main/java/org/guytp/mscexperiment/Ordinals.java
+++ Experiment/app/src/main/java/org/guytp/mscexperiment/Ordinals.java
@@ -6,6 +6,6 @@ package org.guytp.mscexperiment;
 
 public class Ordinals {
 
-    public final static String[] values = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth", "twenty-first", "twenty-second", "twenty-third", "twenty-fourth", "twenty-fifth", "twenty-sixth"};
+    public final static String[] values = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth", "twenty-first", "twenty-second", "twenty-third", "twenty-fourth", "twenty-fifth", "twenty-sixth", "twenty-seventh", "twenty-eighth", "twenty-ninth", "thirtieth", "thirty-first", "thirty-second", "thirty-third", "thirty-forth", "thirty-fifth"};
 
 }
